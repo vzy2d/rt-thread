4bbe987914456fcd68672c21f801164e05d7772c
Zihao Yu <yuzihao@ict.ac.cn>
2019-09-18 01:11:02 +0800

    riscv64-nemu,uart: support uart_getc()

--- bsp/riscv64-nemu/nemu/uart.c
+++ bsp/riscv64-nemu/nemu/uart.c
@@ -1,11 +1,11 @@
 #include "platform.h"
 
-static volatile uint8_t *uart = (void *)0xa10003f8;
+static volatile int8_t *uart = (void *)0xa10003f8;
 
 void uart_putc(uint8_t ch) {
   uart[0] = ch;
 }
 
 int uart_getc(void) {
-  return -1;
+  return uart[0];
 }
