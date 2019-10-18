76b9df188e2eceb37cefa77290d300e9ff809a01
William Wang <zeweiwang@outlook.com>
2019-10-18 22:16:14 +0800

    Added bsp/riscv64-noop

---
+++ bsp/riscv64-noop/nemu/uart.c
@@ -0,0 +1,21 @@
+#include "platform.h"
+
+static volatile uint8_t *uart = (void *)UART0_CTRL_ADDR;
+
+#define REG_QUEUE     0
+#define REG_LINESTAT  5
+#define REG_STATUS_RX 0x01
+#define REG_STATUS_TX 0x20
+
+void uart_putc(uint8_t ch) {
+  while ((uart[REG_LINESTAT] & REG_STATUS_TX) == 0);
+  uart[REG_QUEUE] = ch;
+  if (ch == '\n') uart_putc('\r');
+}
+
+int uart_getc(void) {
+  if (uart[REG_LINESTAT] & REG_STATUS_RX) {
+    return uart[REG_QUEUE];
+  }
+  return -1;
+}
